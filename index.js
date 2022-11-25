bf2d94a4a437f3c1db242c75b14abc64c0701f37
guantai <jpguantai@gmail.com>
2022-11-25 04:39:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+    
+    function upperCaseCustomerName(){
+        customerName = customerName.toUpperCase();
+
+
+    }
+
+function setBestCustomer(){
+
+    bestCustomer= 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer  = 'bob';
+
+
+//function called changeLeastFavoriteCustomer() that attempts to change that constant - notice what JavaScript does when you try to change the constant.
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+    
+}
+
+
+
